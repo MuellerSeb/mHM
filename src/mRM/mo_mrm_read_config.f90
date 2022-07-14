dd2e42785cd7a8853e9bbfa3ea6bc505e320fdb0
muellese <sebastian.mueller@ufz.de>
2022-07-14 17:39:37 +0200

    mrm: use new nml classes to read config

--- src/mRM/mo_mrm_read_config.f90
+++ src/mRM/mo_mrm_read_config.f90
@@ -65,8 +65,13 @@ contains
                                         varnameTotalRunoff, gw_coupling, &
                                         output_deflate_level_mrm, output_double_precision_mrm, &
                                         readLatLon
-    use mo_nml, only : close_nml, open_nml, position_nml
     use mo_string_utils, only : num2str
+    use mo_namelists, only : &
+      nml_mainconfig_mrm, &
+      nml_directories_mrm, &
+      nml_evaluation_gauges, &
+      nml_inflow_gauges, &
+      nml_mrm_outputs
 
     implicit none
 
@@ -78,17 +83,12 @@ contains
     logical, intent(in) :: do_message
 
     integer(i4), dimension(maxNoDomains) :: NoGauges_domain
-
     integer(i4), dimension(maxNoDomains, maxNoGauges) :: Gauge_id
-
     character(256), dimension(maxNoDomains, maxNoGauges) :: Gauge_filename
 
     integer(i4), dimension(maxNoDomains) :: NoInflowGauges_domain
-
     integer(i4), dimension(maxNoDomains, maxNoGauges) :: InflowGauge_id
-
     character(256), dimension(maxNoDomains, maxNoGauges) :: InflowGauge_filename
-
     logical, dimension(maxNoDomains, maxNoGauges) :: InflowGauge_Headwater
 
     integer(i4) :: domainID, iDomain
@@ -98,63 +98,34 @@ contains
     integer(i4) :: idx
 
     character(256), dimension(maxNoDomains) :: dir_Gauges
-
     character(256), dimension(maxNoDomains) :: dir_Total_Runoff
-
     character(256), dimension(maxNoDomains) :: dir_Bankfull_Runoff
 
     logical :: file_exists
 
     type(domainInfo_mRM), pointer :: domain_mrm_iDomain
 
-
-    ! namelist spatial & temporal resolution, optmization information
-    namelist /mainconfig_mrm/ ALMA_convention, &
-      filenameTotalRunoff, varnameTotalRunoff, gw_coupling
-    ! namelist directories
-    namelist /directories_mRM/ dir_Gauges, dir_Total_Runoff, dir_Bankfull_Runoff
-    namelist /evaluation_gauges/ nGaugesTotal, NoGauges_domain, Gauge_id, gauge_filename
-    ! namelist for inflow gauges
-    namelist /inflow_gauges/ nInflowGaugesTotal, NoInflowGauges_domain, InflowGauge_id, &
-            InflowGauge_filename, InflowGauge_Headwater
-    ! name list regarding output
-    namelist /NLoutputResults/ &
-            output_deflate_level_mrm, &
-            output_double_precision_mrm, &
-            timeStep_model_outputs_mrm, &
-            outputFlxState_mrm
-
     !===============================================================
     ! INITIALIZATION
     !===============================================================
     is_start = .True.
-    nGaugesTotal = nodata_i4
-    NoGauges_domain = nodata_i4
-    Gauge_id = nodata_i4
-    gauge_filename = num2str(nodata_i4)
-
-    ! default arguments
-    ALMA_convention = .false.
-    filenameTotalRunoff = 'total_runoff'
-    varnameTotalRunoff = 'total_runoff'
-    gw_coupling = .false.
-
-    !===============================================================
-    !  Read namelist main directories
-    !===============================================================
-    call open_nml(file_namelist, unamelist, quiet = .true.)
 
     !===============================================================
     !  Read namelist for mainconfig for mRM
     !===============================================================
-    call position_nml('mainconfig_mrm', unamelist)
-    read(unamelist, nml = mainconfig_mrm)
+    call nml_mainconfig_mrm%read(file_namelist, unamelist)
+    ALMA_convention = nml_mainconfig_mrm%ALMA_convention
+    filenameTotalRunoff = nml_mainconfig_mrm%filenameTotalRunoff
+    varnameTotalRunoff = nml_mainconfig_mrm%varnameTotalRunoff
+    gw_coupling = nml_mainconfig_mrm%gw_coupling
 
     !===============================================================
     !  Read namelist for mainpaths
     !===============================================================
-    call position_nml('directories_mRM', unamelist)
-    read(unamelist, nml = directories_mRM)
+    call nml_directories_mrm%read(file_namelist, unamelist)
+    dir_Gauges = nml_directories_mrm%dir_Gauges
+    dir_Total_Runoff = nml_directories_mrm%dir_Total_Runoff
+    dir_Bankfull_Runoff = nml_directories_mrm%dir_Bankfull_Runoff
 
     allocate(dirGauges(domainMeta%nDomains), dirTotalRunoff(domainMeta%nDomains), dirBankfullRunoff(domainMeta%nDomains))
     do iDomain = 1, domainMeta%nDomains
@@ -167,8 +138,11 @@ contains
     !===============================================================
     ! READ EVALUATION GAUGES
     !===============================================================
-    call position_nml('evaluation_gauges', unamelist)
-    read(unamelist, nml = evaluation_gauges)
+    call nml_evaluation_gauges%read(file_namelist, unamelist)
+    nGaugesTotal = nml_evaluation_gauges%nGaugesTotal
+    NoGauges_domain = nml_evaluation_gauges%NoGauges_domain
+    Gauge_id = nml_evaluation_gauges%Gauge_id
+    gauge_filename = nml_evaluation_gauges%gauge_filename
 
     if (nGaugesTotal .GT. maxNoGauges) then
       call message()
@@ -258,13 +232,12 @@ contains
     ! Read inflow gauge information
     !===============================================================
 
-    nInflowGaugesTotal = 0
-    NoInflowGauges_domain = 0
-    InflowGauge_id = nodata_i4
-    InflowGauge_filename = num2str(nodata_i4)
-
-    call position_nml('inflow_gauges', unamelist)
-    read(unamelist, nml = inflow_gauges)
+    call nml_inflow_gauges%read(file_namelist, unamelist)
+    nInflowGaugesTotal = nml_inflow_gauges%nInflowGaugesTotal
+    NoInflowGauges_domain = nml_inflow_gauges%NoInflowGauges_domain
+    InflowGauge_id = nml_inflow_gauges%InflowGauge_id
+    InflowGauge_filename = nml_inflow_gauges%InflowGauge_filename
+    InflowGauge_Headwater = nml_inflow_gauges%InflowGauge_Headwater
 
     if (nInflowGaugesTotal .GT. maxNoGauges) then
       call message()
@@ -344,8 +317,6 @@ contains
 
     call common_check_resolution(do_message, .true.)
 
-    call close_nml(unamelist)
-
     !===============================================================
     ! Read namelist global parameters
     !===============================================================
@@ -354,21 +325,12 @@ contains
     !===============================================================
     ! Read Output specifications for mRM
     !===============================================================
-    output_deflate_level_mrm = 6
-    output_double_precision_mrm = .true.
-    outputFlxState_mrm = .FALSE.
-    timeStep_model_outputs_mrm = -2
-    inquire(file = file_defOutput, exist = file_exists)
-    if (file_exists) then
-      ! file exists
-      call open_nml(file_defOutput, udefOutput, quiet = .true.)
-      call position_nml('NLoutputResults', udefOutput)
-      read(udefOutput, nml = NLoutputResults)
-      call close_nml(udefOutput)
-    else
-      call message('')
-      call message('No file specifying mRM output fluxes exists')
-    end if
+    call nml_mrm_outputs%read(file_defOutput, udefOutput)
+    output_deflate_level_mrm = nml_mrm_outputs%output_deflate_level_mrm
+    output_double_precision_mrm = nml_mrm_outputs%output_double_precision_mrm
+    timeStep_model_outputs_mrm = nml_mrm_outputs%timeStep_model_outputs_mrm
+    outputFlxState_mrm = nml_mrm_outputs%outputFlxState_mrm
+
     readLatLon = any(outputFlxState_mrm)
 
     if (any(outputFlxState_mrm)) then
@@ -427,7 +389,10 @@ contains
     use mo_common_functions, only : in_bound
     use mo_common_variables, only : global_parameters, global_parameters_name, processMatrix
     use mo_message, only : message
-    use mo_nml, only : close_nml, open_nml, position_nml
+    use mo_namelists, only : &
+      nml_routing1, &
+      nml_routing2, &
+      nml_routing3
 
     implicit none
 
@@ -444,34 +409,27 @@ contains
     integer(i4) :: start_index
 
     real(dp), dimension(nColPars) :: muskingumTravelTime_constant
-
     real(dp), dimension(nColPars) :: muskingumTravelTime_riverLength
-
     real(dp), dimension(nColPars) :: muskingumTravelTime_riverSlope
-
     real(dp), dimension(nColPars) :: muskingumTravelTime_impervious
-
     real(dp), dimension(nColPars) :: muskingumAttenuation_riverSlope
 
     real(dp), dimension(nColPars) :: streamflow_celerity
     real(dp), dimension(nColPars) :: slope_factor
 
-    namelist /routing1/ muskingumTravelTime_constant, muskingumTravelTime_riverLength, &
-            muskingumTravelTime_riverSlope, muskingumTravelTime_impervious, muskingumAttenuation_riverSlope
-    namelist /routing2/ streamflow_celerity
-    namelist /routing3/ slope_factor
-    !
-    call open_nml(file_namelist_param, unamelist_param, quiet = .true.)
-
     if (processCase .eq. 1_i4) then
-      call position_nml('routing1', unamelist_param)
-      read(unamelist_param, nml = routing1)
+      call nml_routing1%read(file_namelist_param, unamelist_param)
+      muskingumTravelTime_constant = nml_routing1%muskingumTravelTime_constant
+      muskingumTravelTime_riverLength = nml_routing1%muskingumTravelTime_riverLength
+      muskingumTravelTime_riverSlope = nml_routing1%muskingumTravelTime_riverSlope
+      muskingumTravelTime_impervious = nml_routing1%muskingumTravelTime_impervious
+      muskingumAttenuation_riverSlope = nml_routing1%muskingumAttenuation_riverSlope
     else if (processCase .eq. 2_i4) then
-       call position_nml('routing2', unamelist_param)
-       read(unamelist_param, nml = routing2)
+      call nml_routing2%read(file_namelist_param, unamelist_param)
+      streamflow_celerity = nml_routing2%streamflow_celerity
     else if (processCase .eq. 3_i4) then
-       call position_nml('routing3', unamelist_param)
-       read(unamelist_param, nml = routing3)
+      call nml_routing3%read(file_namelist_param, unamelist_param)
+      slope_factor = nml_routing3%slope_factor
     end if
 
     ! -------------------------------------------------------------------------
@@ -525,8 +483,6 @@ contains
               trim(adjustl(file_namelist_param)))
       stop
     end if
-
-    call close_nml(unamelist_param)
 
   end subroutine read_mrm_routing_params
 end module mo_mrm_read_config
