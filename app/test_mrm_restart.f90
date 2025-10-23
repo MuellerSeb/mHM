36a806dd150e79735c8fb54b973aa96bb32d5a69
Matthias Kelbling <matthias.kelbling@ufz.de>
2025-10-23 18:34:14 +0200

    added test for read restart

--- app/test_mrm_restart.f90
+++ app/test_mrm_restart.f90
@@ -10,11 +10,11 @@ program test_mrm
   use mo_string_utils, only: num2str
   use mo_os, only: path_ext, path_isfile
   use mo_message, only: error_message
-  use mo_utils, only: locate
+  use mo_utils, only: locate, is_close
 
   implicit none
 
-  type(river_t), target :: river, criver
+  type(river_t), target :: river, rriver
   type(river_upscaler_t) :: upscaler
   type(grid_t), target :: grid, cgrid
   type(input_dataset) :: ds
@@ -82,6 +82,39 @@ program test_mrm
 
   print*, "write restart file to: river_restart.nc"
   call river%write_restart("river_restart.nc")
+
+  call rriver%read_restart("river_restart.nc")
+
+  print*, "comparing with read restart:"
+  print*, "  n_nodes:", river%n_nodes, rriver%n_nodes
+  print*, "  fdir: size:", (size(river%fdir(:)) .eq. size(rriver%fdir(:))), &
+  " values: ", all(river%fdir .eq. rriver%fdir)
+  print*, "  facc: size:", (size(river%facc(:)) .eq. size(rriver%facc(:))), &
+  " values: ", all(river%facc .eq. rriver%facc)
+  print*, "  down: size:", (size(river%down(:)) .eq. size(rriver%down(:))), &
+  " values: ", all(river%down .eq. rriver%down)
+  print*, "  is_sink: size:", (size(river%is_sink(:)) .eq. size(rriver%is_sink(:))), &
+  " values: ", all(river%is_sink .eqv. rriver%is_sink)
+  print*, "  upstream_area: size:", (size(river%upstream_area(:)) .eq. size(rriver%upstream_area(:))), &
+  " values: ", all(is_close(river%upstream_area, rriver%upstream_area))
+  print*, "  link_length: size:", (size(river%link_length(:)) .eq. size(rriver%link_length(:))), &
+  " values: ", all(is_close(river%link_length, rriver%link_length))
+  print*, "  link_slope: size:", (size(river%link_slope(:)) .eq. size(rriver%link_slope(:))), &
+  " values: ", all(is_close(river%link_slope, rriver%link_slope))
+  print*, "  celerity: size:", (size(river%celerity(:)) .eq. size(rriver%celerity(:))), &
+  " values: ", all(is_close(river%celerity, rriver%celerity))
+  print*, "  node_cell: size:", (size(river%node_cell(:)) .eq. size(rriver%node_cell(:))), &
+  " values: ", all(river%node_cell .eq. rriver%node_cell)
+  print*, "  area_fraction: size:", (size(river%area_fraction(:)) .eq. size(rriver%area_fraction(:))), &
+  " values: ", all(is_close(river%area_fraction, rriver%area_fraction))
+  print*, "  order_id_var: size:", (size(river%order%id(:)) .eq. size(rriver%order%id(:))), &
+  " values: ", all(river%order%id .eq. rriver%order%id)
+  print*, "  order_level_start_var: size:", (size(river%order%level_start(:)) .eq. size(rriver%order%level_start(:))), &
+  " values: ", all(river%order%level_start .eq. rriver%order%level_start)
+  print*, "  order_level_end_var: size:", (size(river%order%level_end(:)) .eq. size(rriver%order%level_end(:))), &
+  " values: ", all(river%order%level_end .eq. rriver%order%level_end)
+  print*, "  order_level_size_var: size:", (size(river%order%level_size(:)) .eq. size(rriver%order%level_size(:))), &
+  " values: ", all(river%order%level_size .eq. rriver%order%level_size)
 
 !   call criver%calc_facc()
 !   if (.not.criver%scc) call criver%export("criver.nc")
